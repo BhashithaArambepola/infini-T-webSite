969e3f2b91ee38358d9acc06b48997b7f7a86797
BhashithaArambepola <barambepola@gmail.com>
2023-02-27 21:38:00 +0530

    change home page ui

---
+++ js/Home.js
@@ -0,0 +1,23 @@
+//---------------clients----------
+$(document).ready(function(){
+  $('.customer-logos').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: true,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+});
--- js/main.js
+++ js/main.js
@@ -186,25 +186,25 @@ $(document).ready(function () {
 
 
 //---------------clients----------
-$(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: false,
-        dots: false,
-        pauseOnHover: true,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
+// $(document).ready(function(){
+//     $('.customer-logos').slick({
+//         slidesToShow: 8,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 1000,
+//         arrows: false,
+//         dots: false,
+//         pauseOnHover: true,
+//         responsive: [{
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 4
+//             }
+//         }, {
+//             breakpoint: 520,
+//             settings: {
+//                 slidesToShow: 3
+//             }
+//         }]
+//     });
+// });
