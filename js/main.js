6df74c08a15528080b1c4c321555c47855fc875a
BhashithaArambepola <barambepola@gmail.com>
2023-04-13 10:30:15 +0530

    restructure css 1

--- js/main.js
+++ js/main.js
@@ -91,32 +91,6 @@ $(document).ready(function () {
 
 /*======Hero section=======*/
 
-//Counter
-$('.counter').each(function () {
-    var $this = $(this),
-        countTo = $this.attr('data-count');
-
-    $({countNum: $this.text()}).animate({
-            countNum: countTo
-        },
-
-        {
-
-            duration: 1000,
-            easing: 'linear',
-            step: function () {
-                $this.text(Math.floor(this.countNum));
-            },
-            complete: function () {
-                $this.text(this.countNum);
-                //alert('finished');
-            }
-
-        });
-
-
-});
-
 
 /*======Services section=======*/
 $(".carousel").owlCarousel({
