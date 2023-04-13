1a6aaea9aff10c070cba6baee50649d93d0dd06c
BhashithaArambepola <barambepola@gmail.com>
2023-04-13 11:09:03 +0530

    restructure css 2

--- js/main.js
+++ js/main.js
@@ -108,20 +108,20 @@ $(".carousel").owlCarousel({
 });
 /*======Contact section=======*/
 
-$(document).ready(function () {
-
-    $('.input').focus(function () {
-        $(this).parent().find(".label-txt").addClass('label-active');
-    });
-
-    $(".input").focusout(function () {
-        if ($(this).val() == '') {
-            $(this).parent().find(".label-txt").removeClass('label-active');
-        }
-        ;
-    });
-
-});
+// $(document).ready(function () {
+//
+//     $('.input').focus(function () {
+//         $(this).parent().find(".label-txt").addClass('label-active');
+//     });
+//
+//     $(".input").focusout(function () {
+//         if ($(this).val() == '') {
+//             $(this).parent().find(".label-txt").removeClass('label-active');
+//         }
+//         ;
+//     });
+//
+// });
 
 //about Us
 
