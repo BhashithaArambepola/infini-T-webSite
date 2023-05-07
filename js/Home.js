1290f4d848f719bdc037ff916e62cf9ec29225a7
BhashithaArambepola <barambepola@gmail.com>
2023-05-07 18:39:53 +0530

    changes 3

--- js/Home.js
+++ js/Home.js
@@ -22,5 +22,53 @@ $(document).ready(function(){
   });
 });
 
+/*======Services section=======*/
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 20000,
+  autoplayHoverPause: false,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false
+    }
+  }
+});
+
+
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
 
 
--- js/main.js
+++ js/main.js
@@ -92,20 +92,7 @@ $(document).ready(function () {
 /*======Hero section=======*/
 
 
-/*======Services section=======*/
-$(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 20000,
-    autoplayHoverPause: false,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false
-        }
-    }
-});
+
 /*======Contact section=======*/
 
 // $(document).ready(function () {
@@ -125,38 +112,6 @@ $(".carousel").owlCarousel({
 
 //about Us
 
-(function () {
-    "use strict";
-
-    var carousels = function () {
-        $(".owl-carousel1").owlCarousel({
-            loop: true,
-            center: true,
-            margin: 0,
-            responsiveClass: true,
-            nav: false,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: false
-                },
-                680: {
-                    items: 2,
-                    nav: false,
-                    loop: false
-                },
-                1000: {
-                    items: 3,
-                    nav: true
-                }
-            }
-        });
-    };
-
-    (function ($) {
-        carousels();
-    })(jQuery);
-})();
 
 
 //---------------clients----------
